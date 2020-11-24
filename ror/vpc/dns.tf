5e252ff8b13cb067cd5da6d4b99006c234759d3d
Martin Fenner <martin.fenner@datacite.org>
2020-11-24 22:56:05 +0100

    name server was set up externally

--- ror/vpc/dns.tf
+++ ror/vpc/dns.tf
@@ -18,18 +18,18 @@ resource "aws_route53_zone" "internal" {
     }
 }
 
-resource "aws_route53_record" "internal-ns" {
-    zone_id = aws_route53_zone.internal.zone_id
-    name = "ror.community"
-    type = "NS"
-    ttl = "30"
-    records = [
-        aws_route53_zone.internal.name_servers.0,
-        aws_route53_zone.internal.name_servers.1,
-        aws_route53_zone.internal.name_servers.2,
-        aws_route53_zone.internal.name_servers.3
-    ]
-}
+// resource "aws_route53_record" "internal-ns" {
+//     zone_id = aws_route53_zone.internal.zone_id
+//     name = "ror.community"
+//     type = "NS"
+//     ttl = "30"
+//     records = [
+//         aws_route53_zone.internal.name_servers.0,
+//         aws_route53_zone.internal.name_servers.1,
+//         aws_route53_zone.internal.name_servers.2,
+//         aws_route53_zone.internal.name_servers.3
+//     ]
+// }
 
 resource "aws_service_discovery_private_dns_namespace" "internal" {
   name = "ror.community"
