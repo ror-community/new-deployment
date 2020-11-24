2189d5e66f83cd6b6e0b2932d56dc94af06b165b
Martin Fenner <martin.fenner@datacite.org>
2020-11-25 00:37:33 +0100

    use correct bucket name

--- ror/services/site/main.tf
+++ ror/services/site/main.tf
@@ -1,5 +1,5 @@
-resource "aws_s3_bucket" "www-ror-community" {
-  bucket = "www.ror.community"
+resource "aws_s3_bucket" "ror-community" {
+  bucket = "ror.community"
   acl = "public-read"
   policy = data.template_file.site.rendered
 
