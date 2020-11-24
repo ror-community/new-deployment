74cebb24bd29d423446b48d040fa9624c73ca636
Martin Fenner <martin.fenner@datacite.org>
2020-11-24 14:46:46 +0100

    enable route 53

--- ror/vpc/dns.tf
+++ ror/vpc/dns.tf
@@ -18,23 +18,23 @@ resource "aws_route53_zone" "internal" {
     }
 }
 
-// resource "aws_route53_record" "internal-ns" {
-//     zone_id = aws_route53_zone.internal.zone_id
-//     name = "ror.community"
-//     type = "NS"
-//     ttl = "30"
-//     records = [
-//         aws_route53_zone.internal.name_servers.0,
-//         aws_route53_zone.internal.name_servers.1,
-//         aws_route53_zone.internal.name_servers.2,
-//         aws_route53_zone.internal.name_servers.3
-//     ]
-// }
+resource "aws_route53_record" "internal-ns" {
+    zone_id = aws_route53_zone.internal.zone_id
+    name = "ror.community"
+    type = "NS"
+    ttl = "30"
+    records = [
+        aws_route53_zone.internal.name_servers.0,
+        aws_route53_zone.internal.name_servers.1,
+        aws_route53_zone.internal.name_servers.2,
+        aws_route53_zone.internal.name_servers.3
+    ]
+}
 
-// resource "aws_service_discovery_private_dns_namespace" "internal" {
-//   name = "ror.community"
-//   vpc = module.vpc.vpc_id
-// }
+resource "aws_service_discovery_private_dns_namespace" "internal" {
+  name = "ror.community"
+  vpc = module.vpc.vpc_id
+}
 
 resource "aws_route53_record" "mx-ror" {
     zone_id = aws_route53_zone.public.zone_id
