bd509c844983dd1e1344a7354e8ea4c100e8dd1c
Martin Fenner <martin.fenner@datacite.org>
2020-11-26 21:20:09 +0100

    don't turn on api cloudfront

--- ror/vpc/cloudfront.tf
+++ ror/vpc/cloudfront.tf
@@ -186,74 +186,74 @@ resource "aws_route53_record" "apex" {
   }
 }
 
-resource "aws_cloudfront_distribution" "api" {
-  origin {
-    domain_name = data.aws_lb.alb.dns_name
-    origin_id = data.aws_lb.alb.id
+// resource "aws_cloudfront_distribution" "api" {
+//   origin {
+//     domain_name = data.aws_lb.alb.dns_name
+//     origin_id = data.aws_lb.alb.id
 
-    custom_origin_config {
-      origin_protocol_policy = "https-only"
-      http_port = "80"
-      https_port = "443"
-      origin_ssl_protocols = ["TLSv1.2", "TLSv1.1", "TLSv1"]
-    }
-  }
+//     custom_origin_config {
+//       origin_protocol_policy = "https-only"
+//       http_port = "80"
+//       https_port = "443"
+//       origin_ssl_protocols = ["TLSv1.2", "TLSv1.1", "TLSv1"]
+//     }
+//   }
 
-  tags = {
-    site        = "ror-api"
-    environment = "production"
-  }
+//   tags = {
+//     site        = "ror-api"
+//     environment = "production"
+//   }
 
-  aliases             = ["api.ror.org"]
-  enabled             = "true"
+//   aliases             = ["api.ror.org"]
+//   enabled             = "true"
 
-  default_cache_behavior {
-    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = data.aws_lb.alb.id
+//   default_cache_behavior {
+//     allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+//     cached_methods   = ["GET", "HEAD"]
+//     target_origin_id = data.aws_lb.alb.id
 
-    forwarded_values {
-      query_string = true
-      headers      = ["Origin", "Host"]
+//     forwarded_values {
+//       query_string = true
+//       headers      = ["Origin", "Host"]
 
-      cookies {
-        forward = "none"
-      }
-    }
+//       cookies {
+//         forward = "none"
+//       }
+//     }
 
-    # This says to redirect http to https
-    viewer_protocol_policy = "redirect-to-https"
-    compress               = "true"
-    min_ttl                = 0
+//     # This says to redirect http to https
+//     viewer_protocol_policy = "redirect-to-https"
+//     compress               = "true"
+//     min_ttl                = 0
 
-    # default cache time in seconds.  This is 1 day, meaning CloudFront will only
-    # look at your S3 bucket for changes once per day.
-    default_ttl            = 86400
-    max_ttl                = 2592000
+//     # default cache time in seconds.  This is 1 day, meaning CloudFront will only
+//     # look at your S3 bucket for changes once per day.
+//     default_ttl            = 86400
+//     max_ttl                = 2592000
 
-    // lambda_function_association {
-    //   event_type   = "origin-request"
-    //   lambda_arn   = aws_lambda_function.index-page.qualified_arn
-    //   include_body = false
-    // }
-  }
+//     // lambda_function_association {
+//     //   event_type   = "origin-request"
+//     //   lambda_arn   = aws_lambda_function.index-page.qualified_arn
+//     //   include_body = false
+//     // }
+//   }
 
-  logging_config {
-    include_cookies = false
-    bucket          = data.aws_s3_bucket.logs.bucket_domain_name
+//   logging_config {
+//     include_cookies = false
+//     bucket          = data.aws_s3_bucket.logs.bucket_domain_name
 
-    prefix = "cf-api/"
-  }
+//     prefix = "cf-api/"
+//   }
 
-  restrictions {
-    geo_restriction {
-      restriction_type = "none"
-    }
-  }
+//   restrictions {
+//     geo_restriction {
+//       restriction_type = "none"
+//     }
+//   }
 
-  viewer_certificate {
-    acm_certificate_arn      = data.aws_acm_certificate.cloudfront.arn
-    ssl_support_method       = "sni-only"
-    minimum_protocol_version = "TLSv1"
-  }
-}
+//   viewer_certificate {
+//     acm_certificate_arn      = data.aws_acm_certificate.cloudfront.arn
+//     ssl_support_method       = "sni-only"
+//     minimum_protocol_version = "TLSv1"
+//   }
+// }
