2bc4ecc2408345622f88b80db5e5844f4a1b8c1b
Martin Fenner <martin.fenner@datacite.org>
2020-12-01 17:13:16 +0000

    update internal dns

--- ror/vpc/dns.tf
+++ ror/vpc/dns.tf
@@ -23,18 +23,19 @@ resource "aws_route53_zone" "internal" {
     }
 }
 
-resource "aws_route53_record" "internal-ns" {
-    zone_id = aws_route53_zone.internal.zone_id
-    name = "ror.org"
-    type = "NS"
-    ttl = "30"
-    records = [
-        aws_route53_zone.internal.name_servers.0,
-        aws_route53_zone.internal.name_servers.1,
-        aws_route53_zone.internal.name_servers.2,
-        aws_route53_zone.internal.name_servers.3
-    ]
-}
+// TODO bring under Terraform control
+// resource "aws_route53_record" "internal-ns" {
+//     zone_id = aws_route53_zone.internal.zone_id
+//     name = "ror.org"
+//     type = "NS"
+//     ttl = "30"
+//     records = [
+//         aws_route53_zone.internal.name_servers.0,
+//         aws_route53_zone.internal.name_servers.1,
+//         aws_route53_zone.internal.name_servers.2,
+//         aws_route53_zone.internal.name_servers.3
+//     ]
+// }
 
 // resource "aws_service_discovery_private_dns_namespace" "internal" {
 //   name = "ror.community"
