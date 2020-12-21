ac8067bc6ea5ab12510b30772170834359c99531
Martin Fenner <martin.fenner@datacite.org>
2020-12-21 23:10:14 +0100

    enable policy attachment

--- ror/vpc/ecs.tf
+++ ror/vpc/ecs.tf
@@ -3,10 +3,10 @@ resource "aws_iam_role" "ecs_tasks_execution_role" {
   assume_role_policy = data.aws_iam_policy_document.ecs_tasks_execution_role.json
 }
 
-// resource "aws_iam_role_policy_attachment" "ecs_tasks_execution_role" {
-//   role       = aws_iam_role.ecs_tasks_execution_role.name
-//   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-// }
+resource "aws_iam_role_policy_attachment" "ecs_tasks_execution_role" {
+  role       = aws_iam_role.ecs_tasks_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
 
 resource "aws_ecs_cluster" "default" {
   name = "default"
