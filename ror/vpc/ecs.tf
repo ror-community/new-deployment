cd01da454a2505306c19b26d3bac22d6748f9e7d
Martin Fenner <martin.fenner@datacite.org>
2020-12-21 22:39:53 +0100

    enable ecs_task_execution_role

--- ror/vpc/ecs.tf
+++ ror/vpc/ecs.tf
@@ -1,7 +1,7 @@
-// resource "aws_iam_role" "ecs_tasks_execution_role" {
-//   name               = "ecs-task-execution-role"
-//   assume_role_policy = data.aws_iam_policy_document.ecs_tasks_execution_role.json
-// }
+resource "aws_iam_role" "ecs_tasks_execution_role" {
+  name               = "ecs-task-execution-role"
+  assume_role_policy = data.aws_iam_policy_document.ecs_tasks_execution_role.json
+}
 
 // resource "aws_iam_role_policy_attachment" "ecs_tasks_execution_role" {
 //   role       = aws_iam_role.ecs_tasks_execution_role.name
