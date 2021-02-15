2382f852698e8d6299e3b8a1fa49b3595ce6afdf
Martin Fenner <martin.fenner@datacite.org>
2021-02-15 17:09:08 +0100

    Update alb-staging.tf

--- ror/vpc/alb-staging.tf
+++ ror/vpc/alb-staging.tf
@@ -12,54 +12,54 @@ module "alb-staging" {
   vpc_id                        = module.vpc.vpc_id
 }
 
-// resource "aws_lb_listener" "alb-http-staging" {
-//   load_balancer_arn = module.alb-staging.this_lb_arn
-//   port              = "80"
-//   protocol          = "HTTP"
+resource "aws_lb_listener" "alb-http-staging" {
+  load_balancer_arn = module.alb-staging.this_lb_arn
+  port              = "80"
+  protocol          = "HTTP"
 
-//   default_action {
-//     target_group_arn = data.aws_lb_target_group.api-community.id
-//     type             = "forward"
-//   }
-// }
+  default_action {
+    target_group_arn = data.aws_lb_target_group.api-community.id
+    type             = "forward"
+  }
+}
 
-// resource "aws_lb_listener" "alb-staging" {
-//   load_balancer_arn = module.alb-staging.this_lb_arn
-//   port              = "443"
-//   protocol          = "HTTPS"
-//   ssl_policy        = "ELBSecurityPolicy-2016-08"
-//   certificate_arn   = data.aws_acm_certificate.ror-staging.arn
+resource "aws_lb_listener" "alb-staging" {
+  load_balancer_arn = module.alb-staging.this_lb_arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = data.aws_acm_certificate.ror-staging.arn
 
-//   default_action {
-//     target_group_arn = data.aws_lb_target_group.api-community.id
-//     type             = "forward"
-//   }
-// }
+  default_action {
+    target_group_arn = data.aws_lb_target_group.api-community.id
+    type             = "forward"
+  }
+}
 
-// resource "aws_lb_listener_rule" "redirect_www-staging" {
-//   listener_arn = aws_lb_listener.alb-staging.arn
+resource "aws_lb_listener_rule" "redirect_www-staging" {
+  listener_arn = aws_lb_listener.alb-staging.arn
 
-//   action {
-//     type = "redirect"
+  action {
+    type = "redirect"
 
-//     redirect {
-//       host        = "www.staging.ror.org"
-//       port        = "443"
-//       protocol    = "HTTPS"
-//       status_code = "HTTP_302"
-//     }
-//   }
+    redirect {
+      host        = "www.staging.ror.org"
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_302"
+    }
+  }
 
-//   condition {
-//     field  = "host-header"
-//     values = ["www.staging.ror.org"]
-//   }
-// }
+  condition {
+    field  = "host-header"
+    values = ["www.staging.ror.org"]
+  }
+}
 
-// resource "aws_route53_record" "www-staging" {
-//     zone_id = data.aws_route53_zone.public.zone_id
-//     name = "www.staging.ror.org"
-//     type = "CNAME"
-//     ttl = var.ttl
-//     records = [data.aws_lb.alb-staging.dns_name]
-// }
+resource "aws_route53_record" "www-staging" {
+    zone_id = data.aws_route53_zone.public.zone_id
+    name = "www.staging.ror.org"
+    type = "CNAME"
+    ttl = var.ttl
+    records = [data.aws_lb.alb-staging.dns_name]
+}
