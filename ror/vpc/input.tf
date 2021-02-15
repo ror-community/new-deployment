9d41c8a66fcc846212b547055e05ba5eea8604c3
Martin Fenner <martin.fenner@datacite.org>
2021-02-15 14:42:33 +0100

    remove obsolete certificate

--- ror/vpc/input.tf
+++ ror/vpc/input.tf
@@ -53,12 +53,6 @@ data "aws_acm_certificate" "ror-staging" {
   most_recent = true
 }
 
-data "aws_acm_certificate" "ror-community" {
-  domain = "ror.community"
-  statuses = ["ISSUED"]
-  most_recent = true
-}
-
 data "aws_acm_certificate" "cloudfront" {
   provider = aws.use1
   domain = "ror.org"
