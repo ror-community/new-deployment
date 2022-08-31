f98487b387bc29d89b8cee580d831addaf18cf9f
lizkrznarich <liz.krznarich@datacite.org>
2022-08-31 12:53:43 -0500

    remove commented code

--- ror/vpc/lambda.tf
+++ ror/vpc/lambda.tf
@@ -1,18 +1,3 @@
-// resource "aws_lambda_function" "routing" {
-//   filename = "routing-runner.js.zip"
-//   function_name = "routing"
-//   role = data.aws_iam_role.lambda.arn
-//   handler = "routing-runner.handler"
-//   runtime = "nodejs6.10"
-//   source_code_hash = base64sha256(file("routing-runner.js.zip"))
-
-//   environment {
-//     variables = {
-
-//     }
-//   }
-// }
-
 resource "aws_lambda_function" "index-page-community" {
   provider = aws.use1
   filename = "index-page-runner.js.zip"
