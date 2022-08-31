b63871ecec2dd08619b349f81bd6904c2deafc02
lizkrznarich <liz.krznarich@datacite.org>
2022-08-31 12:49:10 -0500

    update nodejs to 16 in other lambdas

--- ror/vpc/lambda.tf
+++ ror/vpc/lambda.tf
@@ -19,7 +19,7 @@ resource "aws_lambda_function" "index-page-community" {
   function_name = "index-page-community"
   role = aws_iam_role.iam_for_lambda.arn
   handler = "index-page-runner.handler"
-  runtime = "nodejs14.x"
+  runtime = "nodejs16.x"
   source_code_hash = sha256(filebase64("index-page-runner.js.zip"))
 }
 
@@ -29,6 +29,6 @@ resource "aws_lambda_function" "redirect-community" {
   function_name = "redirect-community"
   role = aws_iam_role.iam_for_lambda.arn
   handler = "redirect-runner.handler"
-  runtime = "nodejs14.x"
+  runtime = "nodejs16.x"
   source_code_hash = sha256(filebase64("redirect-runner.js.zip"))
 }
