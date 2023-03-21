ff5c0527a4538eff17353a8a58f4ec9758039463
lizkrznarich <liz.krznarich@datacite.org>
2023-03-21 18:38:16 -0400

    update lambda function

--- ror/vpc/error-dev.js
+++ ror/vpc/error-dev.js
@@ -1,10 +1,10 @@
 exports.handler = (evt, ctx, cb) => {
+
     const response = {
         statusCode: 200,
         body: JSON.stringify(
         {
             message: 'Let\'s ROR!',
-            input: evt,
         },
         null,
         2
