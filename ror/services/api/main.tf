9a4b33f54014af1e338bbeed95b48297b449b13c
lizkrznarich <liz.krznarich@datacite.org>
2023-04-05 18:45:38 -0500

    add prod autoscaling policy

--- ror/services/api/main.tf
+++ ror/services/api/main.tf
@@ -28,6 +28,30 @@ resource "aws_ecs_service" "api-community" {
   ]
 }
 
+resource "aws_appautoscaling_target" "api-prod-autoscale-target" {
+  max_capacity = 4
+  min_capacity = 2
+  resource_id = "service/default/api-community"
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace = "ecs"
+}
+
+resource "aws_appautoscaling_policy" "api-prod-autoscale-policy" {
+  name = "api-prod-cpu"
+  policy_type = "TargetTrackingScaling"
+  resource_id = aws_appautoscaling_target.api-prod-autoscale-target.resource_id
+  scalable_dimension = aws_appautoscaling_target.api-prod-autoscale-target.scalable_dimension
+  service_namespace = aws_appautoscaling_target.api-prod-autoscale-target.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageCPUUtilization"
+    }
+    target_value = 80
+  }
+}
+
+
 resource "aws_lb_target_group" "api-community" {
   name     = "api-community"
   port     = 80
