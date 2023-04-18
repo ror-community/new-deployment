997c829866d65afb96833738f3d80ccf5599c4ad
lizkrznarich <liz.krznarich@datacite.org>
2023-04-18 10:24:27 -0500

    add new env var for launch darkly

--- ror/services/api/var.tf
+++ ror/services/api/var.tf
@@ -71,3 +71,7 @@ variable "data_store_staging" {}
 variable "route_user" {}
 
 variable "github_token" {}
+
+variable "launch_darkly_key" {}
+variable "launch_darkly_key_dev" {}
+variable "launch_darkly_key_staging" {}
