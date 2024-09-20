f9ccca53b620fc8dc737d6e12d77744ae1a88935
lizkrznarich <liz.krznarich@datacite.org>
2024-09-20 12:42:02 -0500

    remove leo form app dev

--- ror/services/leo-form/input.tf
+++ ror/services/leo-form/input.tf
@@ -5,14 +5,6 @@ provider "aws" {
   version    = "~> 2.7"
 }
 
-provider "aws" {
-  # us-east-1 region
-  access_key = var.access_key
-  secret_key = var.secret_key
-  region = "us-east-1"
-  alias = "use1"
-}
-
 data "aws_route53_zone" "ror" {
   name         = "ror.org"
 }
