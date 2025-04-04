c8874c9de1408842a6110edfa083ac9afb8ef37f
kudakwashe siziva <9620622+kaysiz@users.noreply.github.com>
2025-04-04 16:01:46 +0200

    Add setup for dev db (#199)
    * Initial commit to setup mysql db on dev
    
    * Update org name
    
    * update workspace name
    
    * Add RDS subnet
    
    * trigger terraform plan
    
    * trigger terraform plan
    
    * Update vpc security group
    
    * Fix the service provider for terraform
    
    * Fix the service provider for terraform
    
    * trigger terraform plan
    
    * trigger terraform plan
    
    * trigger terraform plan
    
    * Add input file
    
    * trigger terraform plan

---
+++ ror/data-storage/mysql/var.tf
@@ -0,0 +1,12 @@
+variable "access_key" {}
+variable "secret_key" {}
+variable "region" {
+    default = "eu-west-1"
+}
+
+variable "mysql_user" {}
+variable "mysql_password" {}
+
+variable "private_security_group" {}
+
+variable "private_subnet_ids" {}
