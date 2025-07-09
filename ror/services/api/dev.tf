ecd033bc6aacbd00f3c5d256b3278dc40f3d1947
Justin Barrett <jpbarret13@gmail.com>
2025-07-08 23:30:25 -0400

    API Gateway Testing

--- ror/services/api/dev.tf
+++ ror/services/api/dev.tf
@@ -185,7 +185,7 @@ resource "aws_ecs_service" "api_gateway_test" {
   }
 
   service_registries {
-    registry_arn = aws_service_discovery_service.api-dev-gateway.arn
+    registry_arn = aws_service_discovery_service.api_gateway_test.arn
   }
 
   load_balancer {
@@ -230,8 +230,8 @@ resource "aws_lb_listener_rule" "api_gateway_test" {
 }
 
 
-resource "aws_ecs_task_definition" "api-dev-gateway" {
-  family = "api-dev-gateway"
+resource "aws_ecs_task_definition" "api_gateway_test" {
+  family = "api-gateway-test"
   execution_role_arn = data.aws_iam_role.ecs_tasks_execution_role.arn
   network_mode = "awsvpc"
   requires_compatibilities = ["FARGATE"]
