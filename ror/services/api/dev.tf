137591dcaf01bd0c267a452f52994788b6d8e8e6
Justin Barrett <jpbarret13@gmail.com>
2025-07-08 23:42:05 -0400

    API Gateway Testing

--- ror/services/api/dev.tf
+++ ror/services/api/dev.tf
@@ -356,6 +356,28 @@ resource "aws_api_gateway_method" "v1_organizations_get" {
   authorization = "NONE"
 }
 
+# OPTIONS method for v1/organizations (CORS)
+resource "aws_api_gateway_method" "v1_organizations_options" {
+  rest_api_id   = aws_api_gateway_rest_api.api_gateway_test.id
+  resource_id   = aws_api_gateway_resource.v1_organizations.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+# Method response for v1/organizations
+resource "aws_api_gateway_method_response" "v1_organizations_get" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway_test.id
+  resource_id = aws_api_gateway_resource.v1_organizations.id
+  http_method = aws_api_gateway_method.v1_organizations_get.http_method
+  status_code = "200"
+  
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = true
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+  }
+}
+
 # GET method for v2/organizations
 resource "aws_api_gateway_method" "v2_organizations_get" {
   rest_api_id   = aws_api_gateway_rest_api.api_gateway_test.id
@@ -389,6 +411,32 @@ resource "aws_api_gateway_integration" "v1_organizations_integration" {
   type                    = "HTTP_PROXY"
   integration_http_method = "GET"
   uri                     = "http://${data.aws_lb.alb-dev.dns_name}/v1/organizations/"
+}
+
+# Integration response for v1/organizations
+resource "aws_api_gateway_integration_response" "v1_organizations_integration" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway_test.id
+  resource_id = aws_api_gateway_resource.v1_organizations.id
+  http_method = aws_api_gateway_method.v1_organizations_get.http_method
+  status_code = aws_api_gateway_method_response.v1_organizations_get.status_code
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = "'*'"
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS'"
+  }
+}
+
+# OPTIONS integration for v1/organizations (CORS)
+resource "aws_api_gateway_integration" "v1_organizations_options_integration" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway_test.id
+  resource_id = aws_api_gateway_resource.v1_organizations.id
+  http_method = aws_api_gateway_method.v1_organizations_options.http_method
+
+  type = "MOCK"
+  request_templates = {
+    "application/json" = "{\"statusCode\": 200}"
+  }
 }
 
 # Integration for v2/organizations
