34fca3a24a875239bfa9d03a0aaaf5a414b4f012
Justin Barrett <jpbarret13@gmail.com>
2025-07-12 08:07:19 -0400

    API Gateway Testing

--- ror/services/api/dev.tf
+++ ror/services/api/dev.tf
@@ -217,7 +217,7 @@ resource "aws_lb_target_group" "api_gateway_test" {
 # Listener rule for API Gateway test service - routes all traffic for api-gateway-test.dev.ror.org
 resource "aws_lb_listener_rule" "api_gateway_test_host" {
   listener_arn = data.aws_lb_listener.alb-dev.arn
-  priority = 50
+  priority = 60
 
   action {
     type  = "forward"
@@ -229,8 +229,6 @@ resource "aws_lb_listener_rule" "api_gateway_test_host" {
     values = ["api-gateway-test.dev.ror.org"]
   }
 }
-
-
 
 
 resource "aws_ecs_task_definition" "api_gateway_test" {
