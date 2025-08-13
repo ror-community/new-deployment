67cb4f013d333efa436900644f6215d7db0fa1c1
Justin Barrett <jpbarret13@gmail.com>
2025-08-13 13:53:21 -0400

    Adding dev gateway

--- ror/services/api/dev.tf
+++ ror/services/api/dev.tf
@@ -169,47 +169,6 @@ resource "aws_s3_bucket_policy" "public-dev-bucket-policy" {
 # API GATEWAY DEPLOYMENT & DOMAIN - DEVELOPMENT
 # =============================================================================
 
-# Deployment for development
-resource "aws_api_gateway_deployment" "api_gateway_dev" {
-  depends_on = [
-    aws_api_gateway_integration.dev_v1_organizations_integration,
-    aws_api_gateway_integration.dev_v1_organizations_id_integration,
-    aws_api_gateway_integration.dev_v2_organizations_integration,
-    aws_api_gateway_integration.dev_organizations_integration,
-    aws_api_gateway_integration.dev_v2_organizations_id_integration,
-    aws_api_gateway_integration.dev_organizations_id_integration,
-    aws_api_gateway_integration.dev_v1_organizations_options_integration,
-    aws_api_gateway_integration.dev_v1_organizations_id_options_integration,
-    aws_api_gateway_integration.dev_organizations_id_options_integration,
-    aws_api_gateway_integration.dev_v2_organizations_id_options_integration,
-    aws_api_gateway_integration.dev_v1_heartbeat_integration,
-    aws_api_gateway_integration.dev_v2_heartbeat_integration,
-    aws_api_gateway_integration_response.dev_v1_organizations_integration,
-    aws_api_gateway_integration_response.dev_v1_organizations_id_integration,
-    aws_api_gateway_integration_response.dev_v2_organizations_integration,
-    aws_api_gateway_integration_response.dev_organizations_integration,
-    aws_api_gateway_integration_response.dev_v2_organizations_id_integration,
-    aws_api_gateway_integration_response.dev_organizations_id_integration,
-    aws_api_gateway_integration_response.dev_v1_organizations_options_integration,
-    aws_api_gateway_integration_response.dev_v1_organizations_id_options_integration,
-    aws_api_gateway_integration_response.dev_organizations_id_options_integration,
-    aws_api_gateway_integration_response.dev_v2_organizations_id_options_integration,
-    aws_api_gateway_integration_response.dev_v1_heartbeat_integration,
-    aws_api_gateway_integration_response.dev_v2_heartbeat_integration,
-  ]
-
-  rest_api_id = aws_api_gateway_rest_api.api_gateway_dev.id
-  
-  variables = {
-    deployed_at = timestamp()
-    force_update = "true"
-  }
-  
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
 # API Gateway Custom Domain Name for development
 #resource "aws_api_gateway_domain_name" "api_gateway_dev" {
 #  domain_name = "api.dev.ror.org"
