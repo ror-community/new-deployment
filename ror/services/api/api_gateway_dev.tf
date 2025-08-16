3dee23ddf30597dd87d0bea7ad1e8f909afeb87f
Justin Barrett <jpbarret13@gmail.com>
2025-08-15 23:04:45 -0400

    API Gateway endpoint

--- ror/services/api/api_gateway_dev.tf
+++ ror/services/api/api_gateway_dev.tf
@@ -92,7 +92,7 @@ resource "aws_api_gateway_method_settings" "v2_organizations_cache" {
 resource "aws_api_gateway_method_settings" "organizations_cache" {
   rest_api_id = aws_api_gateway_rest_api.api_gateway.id
   stage_name  = aws_api_gateway_stage.api_gateway_dev.stage_name
-  method_path = "organizations{proxy}/GET"
+  method_path = "organizations/GET"
 
   settings {
     caching_enabled        = true
