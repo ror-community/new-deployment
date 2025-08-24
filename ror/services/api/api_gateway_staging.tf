881aea49600e6ca1cddb2ef6911ba097ec155d85
Justin Barrett <jpbarret13@gmail.com>
2025-08-24 11:57:00 -0400

    API Gateway deployment

--- ror/services/api/api_gateway_staging.tf
+++ ror/services/api/api_gateway_staging.tf
@@ -75,10 +75,10 @@ resource "aws_api_gateway_method_settings" "v1_proxy_cache_staging" {
 }
 
 # Enable caching for v2/organizations endpoint
-resource "aws_api_gateway_method_settings" "v2_organizations_cache_staging" {
+resource "aws_api_gateway_method_settings" "v2_proxy_cache_staging" {
   rest_api_id = aws_api_gateway_rest_api.api_gateway.id
   stage_name  = aws_api_gateway_stage.api_gateway_staging.stage_name
-  method_path = "v2/organizations/GET"
+  method_path = "v2/{proxy+}/GET"
 
   depends_on = [
     aws_api_gateway_method_settings.v1_proxy_cache_staging
@@ -122,32 +122,6 @@ resource "aws_api_gateway_method_settings" "organizations_cache_staging" {
   }
 }
 
-
-
-# Enable caching for v2/organizations/{id} endpoint
-resource "aws_api_gateway_method_settings" "v2_organizations_id_cache_staging" {
-  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
-  stage_name  = aws_api_gateway_stage.api_gateway_staging.stage_name
-  method_path = "v2/organizations/{id}/GET"
-
-  depends_on = [
-    aws_api_gateway_method_settings.organizations_cache_staging
-  ]
-
-  settings {
-    caching_enabled        = true
-    cache_ttl_in_seconds   = 300  # 5 minutes cache TTL
-    cache_data_encrypted   = false
-    
-    # Prevent cache bypass from client headers
-    require_authorization_for_cache_control = true
-    unauthorized_cache_control_header_strategy = "SUCCEED_WITHOUT_RESPONSE_HEADER"
-    
-    throttling_rate_limit  = 10000
-    throttling_burst_limit = 5000
-  }
-}
-
 # Enable caching for organizations/{id} endpoint (no version)
 resource "aws_api_gateway_method_settings" "organizations_id_cache_staging" {
   rest_api_id = aws_api_gateway_rest_api.api_gateway.id
@@ -155,7 +129,7 @@ resource "aws_api_gateway_method_settings" "organizations_id_cache_staging" {
   method_path = "organizations/{id}/GET"
 
   depends_on = [
-    aws_api_gateway_method_settings.v2_organizations_id_cache_staging
+    aws_api_gateway_method_settings.v2_proxy_cache_staging
   ]
 
   settings {
