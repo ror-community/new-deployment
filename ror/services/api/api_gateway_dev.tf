7e11c6c5aa9f32dc3d77f12559231df2a2f8ab55
Justin Barrett <jpbarret13@gmail.com>
2025-09-12 09:40:43 -0400

    API Gateway changes

--- ror/services/api/api_gateway_dev.tf
+++ ror/services/api/api_gateway_dev.tf
@@ -163,7 +163,7 @@ resource "aws_api_gateway_method_settings" "metrics_and_logging" {
 
   settings {
     metrics_enabled             = true
-    logging_level               = "INFO"
+    logging_level               = "ERROR"
     data_trace_enabled         = false
     throttling_rate_limit       = 10000
     throttling_burst_limit      = 5000
