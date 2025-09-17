5f6c8b197fb4e30687c10490910db1c4d2aa1057
Justin Barrett <jpbarret13@gmail.com>
2025-09-17 11:32:47 -0400

    API Gateway changes

--- ror/services/api/main.tf
+++ ror/services/api/main.tf
@@ -88,9 +88,13 @@ resource "aws_ecs_task_definition" "api-community" {
 resource "aws_route53_record" "api" {
     zone_id = data.aws_route53_zone.public.zone_id
     name = "api.ror.org"
-    type = "CNAME"
-    ttl = var.ttl
-    records = [data.aws_lb.alb.dns_name]
+    type = "A"
+    
+    alias {
+      name                   = aws_api_gateway_domain_name.api_gateway_prod.regional_domain_name
+      zone_id                = aws_api_gateway_domain_name.api_gateway_prod.regional_zone_id
+      evaluate_target_health = false
+    }
 }
 
 resource "aws_route53_record" "split-api" {
@@ -215,3 +219,24 @@ resource "aws_cloudwatch_log_resource_policy" "api_gateway_logs_prod" {
   })
 }
 
+# API Gateway Custom Domain Name for production
+resource "aws_api_gateway_domain_name" "api_gateway_prod" {
+  domain_name = "api.ror.org"
+  
+  regional_certificate_arn = data.aws_acm_certificate.ror.arn
+  
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+}
+
+# Base path mapping for API Gateway production custom domain
+resource "aws_api_gateway_base_path_mapping" "api_gateway_prod" {
+  api_id      = aws_api_gateway_rest_api.api_gateway.id
+  stage_name  = aws_api_gateway_stage.api_gateway_prod.stage_name
+  domain_name = aws_api_gateway_domain_name.api_gateway_prod.domain_name
+  
+  depends_on = [
+    aws_api_gateway_stage.api_gateway_prod
+  ]
+}
