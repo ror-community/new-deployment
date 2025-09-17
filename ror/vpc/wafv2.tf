2c4de1c42e35dba51178251cc66eef86c4a161f1
Justin Barrett <jpbarret13@gmail.com>
2025-09-17 12:50:35 -0400

    API Gateway changes

--- ror/vpc/wafv2.tf
+++ ror/vpc/wafv2.tf
@@ -1084,8 +1084,3 @@ resource "aws_wafv2_web_acl" "prod-v2" {
         sampled_requests_enabled   = true
     }
 }
-
-resource "aws_wafv2_web_acl_association" "prod-v2" {
-    resource_arn = data.aws_lb.alb.arn
-    web_acl_arn  = aws_wafv2_web_acl.prod-v2.arn
-}
