da4f98a0b56a7e1a0dd7b539828c045ace673d7c
jrhoads <jrhoads@datacite.org>
2026-09-04 11:16:20 +0200

    refactor: use map(string) for API tag variables

--- ror/services/api/environments/dev/var.tf
+++ ror/services/api/environments/dev/var.tf
@@ -28,7 +28,7 @@ variable "elastic7_port_dev" {
 }
 
 variable "ror-api-dev_tags" {
-  type = "map"
+  type = map(string)
 }
 
 variable "public_key" {}
--- ror/services/api/environments/prod/var.tf
+++ ror/services/api/environments/prod/var.tf
@@ -28,7 +28,7 @@ variable "elastic7_port" {
 }
 
 variable "ror-api_tags" {
-  type = "map"
+  type = map(string)
 }
 
 variable "public_key" {}
